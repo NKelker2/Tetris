3683623406c56aad572a497d44d37766e0250e86
NKelker <168237910+NKelker2@users.noreply.github.com>
2025-12-20 16:43:43 -0500

    Revert "Update Board.cs"
    This reverts commit a17f860aa92ccebe5dab7dcd30a05baa6024d54f.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,16 +1,13 @@
-using System;
 using UnityEngine;
-using UnityEngine.Video;
-using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour {
-    public Tilemap tilemap { get; private set; }
-    public TetrominoData[] tetrominoes;
-    public void Awake() {
-        this.tilemap = GetComponentInChildren<Tilemap>();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start() {
 
-        for (int i = 0; i < tetrominoes.Length; i++) {
-            this.tetrominoes[i].Initialize();
-        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+
     }
 }

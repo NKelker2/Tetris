721609046affc66a549ee44f4daba225c563c25b
NKelker <168237910+NKelker2@users.noreply.github.com>
2025-12-21 21:40:52 -0500

    Implemented wall kicks using tutorial
    prevents blocks from being rotated out of bounds
    
    discrepancy with tutorial at 1:21:15 where his block did not rotate when up against a wall at all

--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -1,4 +1,5 @@
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Piece : MonoBehaviour {
@@ -69,8 +70,18 @@ public class Piece : MonoBehaviour {
     }
 
     private void Rotate(int direction) {
+        int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
 
+        ApplyRotationMatrix(direction);
+
+        if (!TestWallKicks(this.rotationIndex, direction)) {
+            this.rotationIndex = originalRotation;
+            ApplyRotationMatrix(-direction);
+        }
+    }
+
+    private void ApplyRotationMatrix(int direction) {
         for (int i = 0; i < this.cells.Length; i++) {
             Vector3 cell = this.cells[i];
             int x, y;
@@ -92,6 +103,30 @@ public class Piece : MonoBehaviour {
 
             this.cells[i] = new Vector3Int(x, y, 0);
         }
+    }
+
+    private bool TestWallKicks(int rotationIndex, int rotationDirection) {
+        int wallKickIndex = getWallKickIndex(rotationIndex, rotationDirection);
+
+        for (int i = 0; i < data.wallKicks.GetLength(1); i++) {
+            Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
+
+            if (Move(translation)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private int getWallKickIndex(int rotationIndex, int rotationDirection) {
+        int wallKickIndex = rotationIndex * 2;
+
+        if (rotationDirection < 0) {
+            wallKickIndex -= 1;
+        }
+
+        return Wrap(wallKickIndex, 0, data.wallKicks.GetLength(0));
     }
 
     private int Wrap(int input, int min, int max) {

3828b6426f7deab9b7050cff2159fdc5216f4787
NKelker <168237910+NKelker2@users.noreply.github.com>
2026-02-25 17:22:24 -0500

    Update PlayerData.cs

--- Assets/Scripts/Action Log/PlayerData.cs
+++ Assets/Scripts/Action Log/PlayerData.cs
@@ -7,6 +7,7 @@ public static class PlayerData {
     public static Dictionary<String, List<Effect>> onClearEffects = new Dictionary<string, List<Effect>>();
 
     // Tile key will be the color they will spawn on
+    // Might change name to just tokensByColor since all tokens will have effects that activate on clear
     public static Dictionary<Tile, List<Token>> onClearTokens = new Dictionary<Tile, List<Token>>();
     public static Dictionary<TileBase, Token> allTokens = new Dictionary<TileBase, Token>();
 

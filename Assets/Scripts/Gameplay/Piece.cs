f96bd5df73b607a11df0a54e41aac9290f73ac5c
PinaColada <pranavsachdeva1109@gmail.com>
2026-02-25 19:42:51 -0500

    arrow keybinds

--- Assets/Scripts/Gameplay/Piece.cs
+++ Assets/Scripts/Gameplay/Piece.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
-public class Piece : MonoBehaviour {
+public class Piece : MonoBehaviour
+{
 
     public Mirror mirror;
     public TokenTilemap tokenTileMap;
@@ -20,7 +21,8 @@ public class Piece : MonoBehaviour {
 
     public LockTimer lockTimer;
 
-    public void Initialize(Board board, Vector3Int position, TetrominoData data) {
+    public void Initialize(Board board, Vector3Int position, TetrominoData data)
+    {
         this.board = board;
         this.position = position;
         this.data = data;
@@ -28,11 +30,13 @@ public class Piece : MonoBehaviour {
         this.stepTime = Time.time + stepDelay;
         this.lockTime = 0f;
 
-        if (this.cells == null) {
+        if (this.cells == null)
+        {
             cells = new Vector3Int[data.cells.Length];
         }
 
-        for (int i = 0; i < data.cells.Length; i++) {
+        for (int i = 0; i < data.cells.Length; i++)
+        {
             this.cells[i] = (Vector3Int)data.cells[i];
         }
 
@@ -40,7 +44,8 @@ public class Piece : MonoBehaviour {
     }
 
     //built in methods - update happens every game tick; this is pretty much the game loop
-    private void Update() {
+    private void Update()
+    {
         this.board.Clear(this);
 
         grounded = !this.board.IsValidPosition(this, position + Vector3Int.down);
@@ -48,51 +53,63 @@ public class Piece : MonoBehaviour {
         if (grounded)
             this.lockTime += Time.deltaTime;
 
-        if (Input.GetKeyDown(KeyCode.Q)) {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
             Rotate(-1);
         }
-        else if (Input.GetKeyDown(KeyCode.E)) {
+        else if (Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
             Rotate(1);
         }
 
-        if (Input.GetKeyDown(KeyCode.A)) {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
             Move(Vector2Int.left);
         }
-        else if (Input.GetKeyDown(KeyCode.D)) {
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
             Move(Vector2Int.right);
         }
 
-        if (Input.GetKeyDown(KeyCode.S)) {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
             Move(Vector2Int.down);
         }
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
             HardDrop();
         }
 
-        if (Time.time >= this.stepTime) {
+        if (Time.time >= this.stepTime)
+        {
             Step();
         }
 
         this.board.Set(this);
     }
 
-    private void Step() {
+    private void Step()
+    {
         this.stepTime = Time.time + this.stepDelay;
         Move(Vector2Int.down);
-        if (this.lockTime >= this.lockDelay) {
+        if (this.lockTime >= this.lockDelay)
+        {
             Lock();
         }
     }
 
-    private void Lock() {
+    private void Lock()
+    {
         this.board.Set(this);
         this.tokenTileMap.HardSet();
         this.board.ClearLines();
         this.board.SpawnPiece();
     }
 
-    private void HardDrop() {
-        while (Move(Vector2Int.down)) {
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
             continue;
         }
         if (mirror.isActiveAndEnabled)
@@ -101,14 +118,16 @@ public class Piece : MonoBehaviour {
         Lock();
     }
 
-    private bool Move(Vector2Int translation) {
+    private bool Move(Vector2Int translation)
+    {
         Vector3Int newPosition = this.position;
         newPosition.x += translation.x;
         newPosition.y += translation.y;
 
         bool valid = this.board.IsValidPosition(this, newPosition);
 
-        if (valid) {
+        if (valid)
+        {
             this.position = newPosition;
             this.lockTime = 0f;
         }
@@ -116,24 +135,29 @@ public class Piece : MonoBehaviour {
         return valid;
     }
 
-    private void Rotate(int direction) {
+    private void Rotate(int direction)
+    {
         int originalRotation = this.rotationIndex;
         this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
 
         ApplyRotationMatrix(direction);
 
-        if (!TestWallKicks(this.rotationIndex, direction)) {
+        if (!TestWallKicks(this.rotationIndex, direction))
+        {
             this.rotationIndex = originalRotation;
             ApplyRotationMatrix(-direction);
         }
     }
 
-    private void ApplyRotationMatrix(int direction) {
-        for (int i = 0; i < this.cells.Length; i++) {
+    private void ApplyRotationMatrix(int direction)
+    {
+        for (int i = 0; i < this.cells.Length; i++)
+        {
             Vector3 cell = this.cells[i];
             int x, y;
 
-            switch (this.data.tetromino) {
+            switch (this.data.tetromino)
+            {
                 case Tetromino.I:
                 case Tetromino.O:
                     cell.x -= 0.5f;
@@ -152,13 +176,16 @@ public class Piece : MonoBehaviour {
         }
     }
 
-    private bool TestWallKicks(int rotationIndex, int rotationDirection) {
+    private bool TestWallKicks(int rotationIndex, int rotationDirection)
+    {
         int wallKickIndex = getWallKickIndex(rotationIndex, rotationDirection);
 
-        for (int i = 0; i < data.wallKicks.GetLength(1); i++) {
+        for (int i = 0; i < data.wallKicks.GetLength(1); i++)
+        {
             Vector2Int translation = this.data.wallKicks[wallKickIndex, i];
 
-            if (Move(translation)) {
+            if (Move(translation))
+            {
                 return true;
             }
         }
@@ -166,26 +193,32 @@ public class Piece : MonoBehaviour {
         return false;
     }
 
-    private int getWallKickIndex(int rotationIndex, int rotationDirection) {
+    private int getWallKickIndex(int rotationIndex, int rotationDirection)
+    {
         int wallKickIndex = rotationIndex * 2;
 
-        if (rotationDirection < 0) {
+        if (rotationDirection < 0)
+        {
             wallKickIndex -= 1;
         }
 
         return Wrap(wallKickIndex, 0, data.wallKicks.GetLength(0));
     }
 
-    private int Wrap(int input, int min, int max) {
-        if (input < min) {
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
             return max - (min - input) % (max - min);
         }
-        else {
+        else
+        {
             return min + (input - min) % (max - min);
         }
     }
 
-    public float GetLockTime() {
+    public float GetLockTime()
+    {
         return this.lockTime;
     }
 }
